ae0d12180498879261a4c47b22ec25628696a275
Alexander Petree <apetree1001@email.phoenix.edu>
2025-04-13 11:47:48 -0700

    Create Runner.sh

---
+++ .github/workflows/Runner.sh
@@ -0,0 +1,4 @@
+$ curl -o actions
+-runner-linux-x64-2.323.0.tar.gz https://github.com/actions/runner/releases/download/v2.323.0/actions-runner-linux-x64-2.323.0.tar.gz
+$echo"0dbc9bf5a58620fc52cb6cc0448abcca964a8d74b5f39773b7afcad9ab691e19  actions-runner-linux-x64-2.323.0.tar.gz" | shasum -a 
+256 -c $ tar xzf ./actions-runner-linux-x64-2.323.0.tar.gz/config.sh-github.com/Apetree100122/squads_mpl3(${ $A5WRBZ7NURGHWHF6UASYEP3H7QJPW})
